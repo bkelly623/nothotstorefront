d8c6ff6db851b04f3db9f3c85a6c1f84a3723e58
bkelly623 <bkel733@gmail.com>
2025-05-05 14:26:52 -0400

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 	// Update to your storefront URL
 	site: 'https://shop.astro.build',
 	output: 'server',
-	adapter: netlify({ imageCDN: true }),
+	adapter: netlify(), // Removed { imageCDN: true }
 	vite: {
 		build: {
 			assetsInlineLimit(filePath) {
@@ -19,7 +19,6 @@ export default defineConfig({
 		},
 	},
 	image: {
-		// Update to your own image domains
 		domains: [
 			'localhost',
 			'shop-next.astro.build',
@@ -33,62 +32,11 @@ export default defineConfig({
 				STRIPE_SECRET_KEY: envField.string({
 					context: 'server',
 					access: 'secret',
-					// This is a random test key
 					default: 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
 				}),
 				FATHOM_SITE_ID: envField.string({
-					context: 'client',
-					access: 'public',
-					optional: true,
-				}),
-				GOOGLE_GEOLOCATION_SERVER_KEY: envField.string({
 					context: 'server',
-					access: 'secret',
-					optional: true,
-				}),
-				GOOGLE_MAPS_BROWSER_KEY: envField.string({
-					context: 'client',
-					access: 'public',
-					optional: true,
-				}),
-				LOOPS_API_KEY: envField.string({
-					context: 'server',
-					access: 'secret',
-					optional: true,
-				}),
-				LOOPS_SHOP_TRANSACTIONAL_ID: envField.string({
-					context: 'server',
-					access: 'public',
-					optional: true,
-				}),
-				LOOPS_FULFILLMENT_TRANSACTIONAL_ID: envField.string({
-					context: 'server',
-					access: 'public',
-					optional: true,
-				}),
-				LOOPS_FULFILLMENT_EMAIL: envField.string({
-					context: 'server',
-					access: 'public',
-					optional: true,
-				}),
-				// Used by the Astro team for our internal backend
-				SHOP_API_URL: envField.string({
-					context: 'server',
-					access: 'public',
-					optional: true,
-				}),
-				SHOP_API_KEY: envField.string({
-					context: 'server',
-					access: 'secret',
-					optional: true,
-				}),
-				US_SHIPPING_RATE_ID: envField.string({
-					context: 'server',
-					access: 'secret',
-				}),
-				INTERNATIONAL_SHIPPING_RATE_ID: envField.string({
-					context: 'server',
-					access: 'secret',
+					default: '',
 				}),
 			},
 		},
